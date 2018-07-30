6f59d2b0449cc3930f7412d452d1bf3d43799a92
Andy Callahan <amandafillpot@icloud.com>
2018-07-30 15:29:37 -0700

    takehome pt.1 finished

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let userInput = document.getElementById("userInput").value;
+let toDoList = document.getElementById("toDoList");
+let doneList = document.getElementById("doneList");
+let submitBtn = document.getElementById("submitBtn");
+let listArr = [];
+
+submitBtn.addEventListener("click"; (e) => {
+  listArr.push(userInput);
+})
