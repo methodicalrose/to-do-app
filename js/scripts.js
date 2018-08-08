5c2c4fd535c017a80a9a6e81c0495c2526936e5f
Andy Callahan <amandafillpot@icloud.com>
2018-08-08 16:30:17 -0700

    working through functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-let userInput = document.getElementById("userInput").value;
+let inputForm = document.forms.userInput;
+let userInput = document.forms.userInput.value;
 let toDoList = document.getElementById("toDoList");
 let doneList = document.getElementById("doneList");
-let submitBtn = document.getElementById("submitBtn");
 let listArr = [];
+let doneListArr = [];
 
-submitBtn.addEventListener("click"; (e) => {
+inputForm.addEventListener("submit"; (e) => {
   listArr.push(userInput);
-})
+  for(let listItem of listArr) {
+    let newItem = toDoList.createNewElement("li");
+    newItem.textContent = listItem;
+  };
+});
